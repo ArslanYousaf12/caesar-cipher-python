8b25a61f3fd2386448b90f83448057268dc5bc47
Arslan Yousaf <124046832+ArslanYousaf12@users.noreply.github.com>
2025-02-18 10:22:36 +0500

    initial commit with keyword argument function

---
+++ main.py
@@ -0,0 +1,17 @@
+def greet():
+    print("hello How are you")
+    print("How you doing")
+
+def greetWithName(name):
+    print(f"Hello {name}")
+    print(f"how are You {name}")
+
+def greetWith(name, location):
+    print(f"Hi Hello {name}")
+    print(f"What is it like to live in {location}")
+
+
+greetWith(location= "Landon",name= "Angela")
+
+# greet()
+# greetWithName("Angela")
