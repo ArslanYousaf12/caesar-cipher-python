99d71e945ecac1cf0c341ee3caf3e61a5943e351
Arslan Yousaf <124046832+ArslanYousaf12@users.noreply.github.com>
2025-02-20 11:13:47 +0500

    convert the two function into one function

--- main.py
+++ main.py
@@ -5,26 +5,37 @@ text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 #TODO-1: Combine the encrypt() and decrypt() functions into a single function called caesar(). 
+def caesar(text, shift, direction):
+  converted_text = ""
+  for letter in text:
+      position = alphabet.index(letter)
+      if direction == "encode":
+        new_position = position + shift
+      else:
+        new_position = position  - shift
+      converted_text += alphabet[new_position]
+  print(f"The Converted test is {converted_text}")
+# def encrypt(plain_text, shift_amount):
+#   cipher_text = ""
+#   for letter in plain_text:
+#     position = alphabet.index(letter)
+#     new_position = position + shift_amount
+#     cipher_text += alphabet[new_position]
+#   print(f"The encoded text is {cipher_text}")
 
-def encrypt(plain_text, shift_amount):
-  cipher_text = ""
-  for letter in plain_text:
-    position = alphabet.index(letter)
-    new_position = position + shift_amount
-    cipher_text += alphabet[new_position]
-  print(f"The encoded text is {cipher_text}")
+# def decrypt(cipher_text, shift_amount):
+#   plain_text = ""
+#   for letter in cipher_text:
+#     position = alphabet.index(letter)
+#     new_position = position - shift_amount
+#     plain_text += alphabet[new_position]
+#   print(f"The decoded text is {plain_text}")
 
-def decrypt(cipher_text, shift_amount):
-  plain_text = ""
-  for letter in cipher_text:
-    position = alphabet.index(letter)
-    new_position = position - shift_amount
-    plain_text += alphabet[new_position]
-  print(f"The decoded text is {plain_text}")
+# if direction == "encode":
+#   encrypt(plain_text=text, shift_amount=shift)
+# elif direction == "decode":
+#   decrypt(cipher_text=text, shift_amount=shift)
 
-if direction == "encode":
-  encrypt(plain_text=text, shift_amount=shift)
-elif direction == "decode":
-  decrypt(cipher_text=text, shift_amount=shift)
-
-#TODO-2: Call the caesar() function, passing over the 'text', 'shift' and 'direction' values.
+#TODO-2: Call the caesar() function, passing over the 'text',
+#  'shift' and 'direction' values.
+caesar(text= text, shift= shift, direction= direction)
